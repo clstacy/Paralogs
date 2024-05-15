adf68c059d5324a50e2cb8ca460aaee76f6be033
Carson Stacy <clstacy@uark.edu>
2024-05-15 14:35:21 -0500

    Version 0.0.1
    Upload of first version, working

---
+++ R/Paralogs-package.R
@@ -0,0 +1,21 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import ggplot2
+#' @importFrom AnnotationDbi select
+#' @importFrom dplyr filter
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom edgeR topTags
+#' @importFrom ggkegg ggkegg
+#' @importFrom ggkegg overlay_raw_map
+#' @importFrom KEGGREST keggList
+#' @importFrom magrittr %>%
+#' @importFrom stringr str_remove_all
+#' @importFrom stringr str_split
+#' @importFrom tibble rownames_to_column
+#' @importFrom tidyr separate_rows
+## usethis namespace: end
+NULL
