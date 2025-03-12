f59f1e2d35034bf62c13f764821379300c939a88
Carson Stacy <clstacy@uark.edu>
2025-03-12 16:46:23 -0500

    continued testing

--- R/paralogs.R
+++ R/paralogs.R
@@ -103,7 +103,7 @@ getPathwayNumber <- function(kegg_results, pathway_id) {
 #' @export
 create_ggkegg <- function(kegg_results, pathway_id, pathway_number, organism_code = "sce") {
   KEGG_data <- kegg_results %>%
-    ggkegg::ggkegg(.,
+    ggkegg(.,
       layout = "native",
       return_tbl_graph = TRUE,
       convert_first = FALSE,
